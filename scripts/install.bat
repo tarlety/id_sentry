ad3477114a3abc94e145bb3607febf349402c16f
tarlety <tarlety@users.noreply.github.com>
2020-04-05 15:22:38 +0800

    save release published scripts in repo.

---
+++ scripts/install.bat
@@ -0,0 +1,7 @@
+@echo on
+
+set BATCHDIR="%~dp0"
+
+mkdir "%APPDATA%\ElectronIDSentry\"
+copy %BATCHDIR%"\id-sentry-config.json" "%APPDATA%\ElectronIDSentry\"
+copy %BATCHDIR%"\ElectronIDSentry.1.0.0.exe" "%USERPROFILE%\Desktop\"
---
+++ scripts/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp id-sentry-config.json ~/.config/ElectronIDSentry/
