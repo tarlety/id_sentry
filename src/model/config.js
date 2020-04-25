30fc78fb29b747b4d064e131c75e2592255c5fda
tarlety <tarlety@users.noreply.github.com>
2020-04-25 22:01:09 +0800

    fix config.js lint error.

--- src/model/config.js
+++ src/model/config.js
@@ -1,35 +1,37 @@
+const Store = require('electron-store');
 const { UID_FORMAT } = require('../js/uid');
 const hash = require('../js/challenge');
-const Store = require('electron-store');
 
-const config_store = new Store({
+const store = new Store({
   name: 'id-sentry-config',
 });
 
-let config = config_store.get('config');
+let config = store.get('config');
 
 if (!config) {
-  const default_node_id = '000-000-00';
-  const default_nonce = 'ABD910FE';
+  const defaults = {
+    node_id: '000-000-00',
+    nonce: 'ABD910FE',
+  };
 
   config = {
     version: 1,
-    node_id: default_node_id,
-    nonce: default_nonce,
+    node_id: defaults.node_id,
+    nonce: defaults.nonce,
     login_ids_hash: ['A123456789'].map((id) =>
-      hash(default_node_id + default_nonce + id)
+      hash(defaults.node_id + defaults.nonce + id)
     ),
     login_uids_hash: ['FFFFFFFE', '00112233'].map((uid) =>
-      hash(default_node_id + default_nonce + uid)
+      hash(defaults.node_id + defaults.nonce + uid)
     ),
-    login_pwd_hash: hash(default_node_id + default_nonce + 'PLZIGNOREME'),
+    login_pwd_hash: hash(`${defaults.node_id + defaults.nonce}PLZIGNOREME`),
     reader_type: UID_FORMAT.LE_HEX,
     enable_hash_data: false,
     enable_encrypt_data: false,
     prod: false,
   };
 
-  config_store.set('config', config);
+  store.set('config', config);
 }
 
 module.exports = config;
